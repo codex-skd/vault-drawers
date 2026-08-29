0d3457a44e6af6bf2d5fa81e6883d20cad8f7c13
Mods Minecraft CI <ci@mods-minecraft.dev>
2026-08-29 18:11:49 +0000

    chore: sync public code from 7f713cf9

--- src/main/java/com/stalkingdragons/minecraft/vaultdrawers/core/ModRecipes.java
+++ src/main/java/com/stalkingdragons/minecraft/vaultdrawers/core/ModRecipes.java
@@ -15,16 +15,22 @@ public class ModRecipes
 {
     private static final ChameleonRegistry<RecipeSerializer<?>> RECIPES = ChameleonServices.REGISTRY.create(BuiltInRegistries.RECIPE_SERIALIZER, ModConstants.MOD_ID);
 
+    private static final AddUpgradeRecipe ADD_UPGRADE = new AddUpgradeRecipe();
+    private static final KeyringRecipe KEYRING = new KeyringRecipe();
+    private static final RemoteGroupUpgradeRecipe REMOTE_GROUP_UPGRADE = new RemoteGroupUpgradeRecipe();
+    private static final UpgradeDetachedDrawerRecipe DETACHED_UPGRADE = new UpgradeDetachedDrawerRecipe();
+    private static final PersonalKeyRecipe PERSONAL_KEY = new PersonalKeyRecipe();
+
     public static final RegistryEntry<RecipeSerializer<AddUpgradeRecipe>> UPGRADE_RECIPE_SERIALIZER = RECIPES.register("add_upgrade",
-        () -> new RecipeSerializer<>(MapCodec.unit(AddUpgradeRecipe::new), StreamCodec.unit(new AddUpgradeRecipe())));
+        () -> new RecipeSerializer<>(MapCodec.unit(ADD_UPGRADE), StreamCodec.unit(ADD_UPGRADE)));
     public static final RegistryEntry<RecipeSerializer<KeyringRecipe>> KEYRING_RECIPE_SERIALIZER = RECIPES.register("keyring",
-        () -> new RecipeSerializer<>(MapCodec.unit(KeyringRecipe::new), StreamCodec.unit(new KeyringRecipe())));
+        () -> new RecipeSerializer<>(MapCodec.unit(KEYRING), StreamCodec.unit(KEYRING)));
     public static final RegistryEntry<RecipeSerializer<RemoteGroupUpgradeRecipe>> REMOTE_GROUP_UPGRADE_SERIALIZER = RECIPES.register("remote_group_upgrade",
-        () -> new RecipeSerializer<>(MapCodec.unit(RemoteGroupUpgradeRecipe::new), StreamCodec.unit(new RemoteGroupUpgradeRecipe())));
+        () -> new RecipeSerializer<>(MapCodec.unit(REMOTE_GROUP_UPGRADE), StreamCodec.unit(REMOTE_GROUP_UPGRADE)));
     public static final RegistryEntry<RecipeSerializer<UpgradeDetachedDrawerRecipe>> DETACHED_UPGRADE_RECIPE_SERIALIZER = RECIPES.register("add_detached_upgrade",
-        () -> new RecipeSerializer<>(MapCodec.unit(UpgradeDetachedDrawerRecipe::new), StreamCodec.unit(new UpgradeDetachedDrawerRecipe())));
+        () -> new RecipeSerializer<>(MapCodec.unit(DETACHED_UPGRADE), StreamCodec.unit(DETACHED_UPGRADE)));
     public static final RegistryEntry<RecipeSerializer<PersonalKeyRecipe>> PERSONAL_KEY_RECIPE_SERIALIZER = RECIPES.register("personal_key_cycle",
-        () -> new RecipeSerializer<>(MapCodec.unit(PersonalKeyRecipe::new), StreamCodec.unit(new PersonalKeyRecipe())));
+        () -> new RecipeSerializer<>(MapCodec.unit(PERSONAL_KEY), StreamCodec.unit(PERSONAL_KEY)));
 
     public static void init (ChameleonInit.InitContext context) {
         RECIPES.init(context);
